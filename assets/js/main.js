15bcbd3ca09bdcb00c4f387697e5e69e12c7cf90
verasantiwijaya <verasantiwijaya@gmail.com>
2024-03-07 17:19:41 +0700

    website

---
+++ assets/js/main.js
@@ -0,0 +1,133 @@
+// NAVIGATION BAR FUNCTION
+    function myMenuFunction(){
+        var menuBtn = document.getElementById("myNavMenu");
+
+        if(menuBtn.className ==="nav-menu"){
+            menuBtn.className += " responsive";
+        } else{
+            menuBtn.className = "nav-menu";
+        }
+    }
+
+// ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING
+    window.onscroll = function() {headerShadow()};
+
+    function headerShadow() {
+        const navHeader = document.getElementById("header");
+
+        if(document.body.scrollTop > 50 || document.documentElement.scrollTop >50) {
+            
+            navHeader.style.boxShadow = "0 1px 6px rgba(0,0,0,0.1)";
+            navHeader.style.height = "70px";
+            navHeader.style.lineHeight = "70px";
+        
+        } else {
+            navHeader.style.boxShadow = "none";
+            navHeader.style.height = "90px";
+            navHeader.style.lineHeight = "90px";
+        }
+    }
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText",{
+    strings : ["Student","Programmer", "Designer"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+function scrollActive() {
+  const scrollY = window.scrollY;
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }  else {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+
+/* ----- SLIDER CERTICATE ----- */
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+}
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+window.addEventListener('scroll', scrollActive)
